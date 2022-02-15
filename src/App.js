be2af47b2b98c0a56b364c16159537f1263cf344
Mayra <mayra203@gmail.com>
2022-02-15 16:59:22 -0600

    Agregando modal, funciones para ocultar o mostrar el modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShow] = useState(false)
+  const showCartHandler = () =>{
+    setCartIsShow(true)
+  }
+  const hideCartHandler = () =>{
+    setCartIsShow(false)
+  }
   return (
     <Fragment>
-      <Header />
+      { cartIsShown && <Cart onClose={hideCartHandler} /> }
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
