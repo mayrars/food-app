bdc58795c9ad6ea1eb83c979579d186a26c67e48
Mayra <mayra203@gmail.com>
2022-02-17 17:02:21 -0600

    Ajuste de los nombres

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import Cart from "./components/Cart/Cart";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 import CartProvider from './store/CartProvider';
 
 function App() {
-  const [cartIsShown, setCartIsShow] = useState(false)
+  const [cartIsShown, setCartIsShown] = useState(false);
   const showCartHandler = () =>{
-    setCartIsShow(true)
+    setCartIsShown(true)
   }
   const hideCartHandler = () =>{
-    setCartIsShow(false)
+    setCartIsShown(false)
   }
   return (
     <CartProvider>
