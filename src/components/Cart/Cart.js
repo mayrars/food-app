a9e3a924308aec9dcec11ef9225dcb8a64ec1dc0
Mayra <mayra203@gmail.com>
2022-03-04 17:31:16 -0600

    Agregando validación del formulario al ordenar, se agrega formulario en el carrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,13 +11,16 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
-  };
+  }
+
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
-  };
+  }
+
   const orderHandler = () => {
     setIsCheckout(true);
-  };
+  }
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
